3b8f968f23d8521c6be5cf53f657210ec2dfacc1
clempers <dclemson@andrew.cmu.edu>
2017-03-02 09:51:17 -0500

    Shield Wall

--- Drones/Assets/PylonDrone.cs
+++ Drones/Assets/PylonDrone.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 public class PylonDrone : Drone {
     public Vector3 bottom_offset;
 
+    public Vector3 shield_direction;
+
     public void move_bottom_towards(float t, Vector3 location)
     {
         move_towards(t, location + -bottom_offset);
+    }
+
+    public void aim_at_pylon(Vector3 pylon)
+    {
+        transform.rotation = Quaternion.FromToRotation(shield_direction, pylon - transform.position);
     }
 }
--- Drones/Assets/ShieldWallFormation.cs
+++ Drones/Assets/ShieldWallFormation.cs
@@ -10,6 +10,10 @@ public class ShieldWallFormation : Formation {
     public PylonDrone pylon2;
     public Vector3 pylon2_location;
 
+    public Shield shield;
+
+    public Vector3 shieldNormal;
+
     public Player player;
 
 
@@ -54,5 +58,14 @@ public class ShieldWallFormation : Formation {
             pylon1.move_bottom_towards(Time.deltaTime, pylon1_location);
         if (pylon2 != null)
             pylon2.move_bottom_towards(Time.deltaTime, pylon2_location);
+
+        if(pylon1 != null && pylon2 != null  && shield != null)
+        {
+            pylon1.aim_at_pylon(pylon2.transform.position);
+            pylon2.aim_at_pylon(pylon1.transform.position);
+            shield.transform.position = Vector3.Lerp(pylon1.transform.position, pylon2.transform.position, 0.5f);
+            shield.transform.rotation = Quaternion.FromToRotation(shieldNormal, Vector3.Cross(new Vector3(0f, 1f, 0f), pylon1.transform.position - pylon2.transform.position));
+            shield.transform.localScale = new Vector3(Vector3.Distance(pylon1.transform.position,pylon2.transform.position), shield.transform.localScale.y, shield.transform.localScale.z);
+        }
     }
 }
